5b2db70fe6c383c4e448083abb0482e2a252d351
Romuald Brillout <git@brillout.com>
2022-02-28 15:15:38 +0100

    wip

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,6 +2,12 @@ import react from "@vitejs/plugin-react";
 import ssr from "vite-plugin-ssr/plugin";
 
 export default {
-  //plugins: [react(), ssr({ pageFiles: { include: ["framework"] } })],
-  plugins: [react(), ssr({ pageFiles: { include: ["node_modules/framework/"] } })],
+  plugins: [
+    react(),
+    ssr({
+      pageFiles: {
+        include: ["framework"],
+      },
+    }),
+  ],
 };
